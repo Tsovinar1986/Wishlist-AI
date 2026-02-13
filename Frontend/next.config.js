6233270e03a2ff827e0503305aee6e9e8926daff
Tsovinar1986 <babakhanyan.tsovinar1986@gmail.com>
2026-02-13 13:44:45 +0400

    https://github.com/Tsovinar1986/WISHLIST-AI.git

--- Frontend/next.config.js
+++ Frontend/next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "standalone",
+  // Allow dev server access from other devices on the network (e.g. http://192.168.x.x:3000)
+  allowedDevOrigins: ["http://192.168.27.7:3000", "http://localhost:3000"],
 };
 
 module.exports = nextConfig;
